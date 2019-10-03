9a67a38ada18f8e29e906905f639808d2f178a93
Raghu Betina <raghu@firstdraft.com>
2019-10-03 21:26:36 +0000

    Direct association between users and user_inventories

--- app/models/user_inventory.rb
+++ app/models/user_inventory.rb
@@ -1,6 +1,8 @@
 class UserInventory < ApplicationRecord
   # Direct associations
 
+  belongs_to :user
+
   # Indirect associations
 
   # Validations
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,8 @@ RSpec.describe User, type: :model do
   
     describe "Direct Associations" do
 
+    it { should have_many(:user_inventories) }
+
     it { should have_many(:favorites) }
 
     end
