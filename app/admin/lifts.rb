1c8e59a83128642c8e79ba4d93852cec886a08e5
Raghu Betina <raghu@firstdraft.com>
2019-10-03 21:27:01 +0000

    Whitelist columns in Lift for ActiveAdmin

--- app/admin/lifts.rb
+++ app/admin/lifts.rb
@@ -1,4 +1,6 @@
 ActiveAdmin.register Lift do
+
+ permit_params :cooldown_hours, :name
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
